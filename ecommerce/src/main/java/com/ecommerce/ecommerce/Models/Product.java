b69093edbc812cc78434e5f145362715bb3ecc41
abdelrahmanmohsen-dev <abdelrahmanmohsen289@gmail.com>
2026-07-06 01:45:41 +0300

    .

--- ecommerce/src/main/java/com/ecommerce/ecommerce/Models/Product.java
+++ ecommerce/src/main/java/com/ecommerce/ecommerce/Models/Product.java
@@ -33,7 +33,7 @@ import lombok.ToString;
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long productId;
 
     @NotBlank
